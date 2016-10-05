4b36ab5aa15ee6eef79a1ed826ba23caf4d1db4f
Sam Fitz <abigwonderful@gmail.com>
2016-10-05 13:09:46 -0600

    update footer

--- web/packages/sequence/themes/sequence/elements/footer.php
+++ web/packages/sequence/themes/sequence/elements/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    TitleCard Capital&trade;. Copyright &copy; <?php echo date('Y'); ?> |
+    TitleCard Capital&reg;. Copyright &copy; <?php echo date('Y'); ?> |
     <!--<a modalize="<?php echo URL::route(array('/terms_of_use', 'sequence')); ?>">Terms Of Use</a> |-->
     <a href="<?php echo View::url('/terms-of-use'); ?>">Terms Of Use</a> |
     <a href="<?php echo View::url('/privacy-policy'); ?>">Privacy Policy</a>
